424d213c456b068ccdb34a9ae698198da95470e5
Surachhya Adhikari <sadhika4@studens.towson.edu>
2025-09-10 19:57:02 -0400

    Comparison operations

--- main.rs
+++ main.rs
@@ -1,16 +1,14 @@
 fn main() {
-    let a = true;
-    let b = false;
+    let a = 1;
+    let b = 2;
     println!("a is {} and b is {}", a, b);
-    println!("not a is {}", !a);
-    println!("a and b is {}", a && b);
-    println!("a or b is {}", a | b);
-    println!("a xor b is {}", a ^ b);
-
-    let c = ( a ^ b) && panic!(); //( a ^ b) || (a & b);
-    println!("c is {}", c);
+    println!("a EQUALS b is {}", a == b);
+    println!("a NOT EQUALS b is {}", a != b);
+    println!("a GREATER THAN b is {}", a > b);
+    println!("a GREATER THAN OR EQUAL TO b is {}", a >= b);
+    println!("a LESS THAN OR EQUAL TO b is {}", a <= b);
+    println!("a LESS THAN b is {}", a < b);
 }
-
 
 
 // my hello world program
@@ -88,6 +86,19 @@ fn main() {
     value = value << 2;
     println!("value is {:08b}" , value);
 }
+
+Boolean data type & operations
+fn main() {
+    let a = true;
+    let b = false;
+    println!("a is {} and b is {}", a, b);
+    println!("not a is {}", !a);
+    println!("a and b is {}", a && b);
+    println!("a or b is {}", a | b);
+    println!("a xor b is {}", a ^ b);
+
+    let c = ( a ^ b) && panic!(); //( a ^ b) || (a & b);
+    println!("c is {}", c);
 }
 
  */
