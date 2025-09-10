b3054b86d199ef40d693224429e6d4901697028e
Surachhya Adhikari <sadhika4@studens.towson.edu>
2025-09-10 19:05:08 -0400

    Declaring variables

--- main.rs
+++ main.rs
@@ -1,3 +1,16 @@
-fn main() {
+//my hello world program
+
+ /*
+ fn main() {
     println!("Hello, world!");
 }
+ */
+
+//declaring variables
+fn main() {
+    let mut x = 10;
+    println!("x is {}", x);
+    x = 20;
+    println!("x is {}", x);
+}
+
