ddec1910545081071e9e2a8e12d4dafc811e5c6a
Surachhya Adhikari <sadhika4@studens.towson.edu>
2025-09-10 19:20:07 -0400

    Integer data types

--- main.rs
+++ main.rs
@@ -1,16 +1,26 @@
-//my hello world program
+// my hello world program
 
  /*
  fn main() {
     println!("Hello, world!");
 }
- */
-
+ 
+*/
 //declaring variables
+/*
 fn main() {
     let mut x = 10;
     println!("x is {}", x);
     x = 20;
     println!("x is {}", x);
 }
+ */
 
+    
+//Integer data types
+fn main() {
+    let mut x: u8 = 255;
+    x = x +1 ;
+    println!("x is {}", x);
+    
+} 
