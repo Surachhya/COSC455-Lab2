4facf47efcda1a9fc7cb9f818379b9fd1d042c86
Surachhya Adhikari <sadhika4@studens.towson.edu>
2025-09-10 21:41:29 -0400

    Chapter:5 Challenge solution

--- main.rs
+++ main.rs
@@ -1,15 +1,27 @@
 fn main(){
-    let mut matrix = [[1,2,3], [4,5,6], [7,8,9]];
+    let numbers = [1, 9, -2, 0, 23, 20, -7, 13, 37, 20, 56, -18, 20, 3];
+    let mut max: i32 ;
+    let mut min: i32 ;
+    let mut mean: f64 ;
 
-    for row in matrix.iter_mut(){
-        for num in row.iter_mut(){
-            *num += 10;
-            print!("{}\t ", num);
+    max = numbers[0];
+    min = numbers[0];
+    mean = 0.0;
 
-        
+    for &num in numbers.iter(){
+        if num > max {
+            max = num;
         }
-        println!();
+        else if num < min {
+            min = num;
+        }
+        mean += num as f64;
     }
+    mean/= numbers.len() as f64;
+    assert_eq!(max, 56);
+    assert_eq!(min, -18);
+    assert_eq!(mean, 12.5);
+    println!("Test passed");
 }
 
 
@@ -91,7 +103,20 @@ fn main(){
         println!("number is {}", number);
     }
 }
- 
+ Nested loops
+ fn main(){
+    let mut matrix = [[1,2,3], [4,5,6], [7,8,9]];
+
+    for row in matrix.iter_mut(){
+        for num in row.iter_mut(){
+            *num += 10;
+            print!("{}\t ", num);
+
+        
+        }
+        println!();
+    }
+}
 my hello world program
  fn main() {
     println!("Hello, world!");
