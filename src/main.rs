905c14069548b4378f9a5cd69cfe841ca0cda874
Surachhya Adhikari <sadhika4@studens.towson.edu>
2025-09-21 13:23:10 -0400

    Module10: Challenge

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,31 @@
-struct Color (u8, u8, u8); //RGB
+struct Rectangle{
+    width: f64,
+    height: f64
+}
+impl Rectangle {
+    fn get_area(&self)-> f64 {
+        self.width * self.height
+    }
+    fn scale(&mut self, scalar:f64){
+        self.width *= scalar;
+        self.height *= scalar;
+    }
+    fn new (width: f64, height: f64) -> Rectangle{
+        Rectangle {
+            width,
+            height
+        }
+}
+}
+fn main(){
+    let mut rect = Rectangle::new(1.2, 3.4);
+    assert_eq!(rect.get_area(), 4.08);
+    rect.scale(0.5);
+    assert_eq!(rect.get_area(), 1.02);
+    println!("Test passed");
+}
+
+/* struct Color (u8, u8, u8); //RGB
 struct Point(u8, u8, u8); //XYZ
 
 fn get_y(p: Point) -> u8 {
@@ -15,7 +42,7 @@ fn main(){
 }
 
 
-/*struct Shuttle {
+struct Shuttle {
     name: String,
     crew_size: u8,
     propellant: f64
